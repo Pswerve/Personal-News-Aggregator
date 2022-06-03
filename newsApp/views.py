50eb67b16c33ffb85ff5825873d482683a764f6d
Prajwal S <ps.swerve@gmail.com>
2022-06-03 11:15:31 +0530

    Add files via upload

---
+++ newsApp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from newsapi.newsapi_client import NewsApiClient
+import requests
+API_KEY = '292dc90f1c074f319030cea2c48c1449'
+
+
+# Create your views here.
+
+
+def home(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'newsApp/home.html', context)
